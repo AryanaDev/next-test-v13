ca23cfcb8000f778eb0dcb597afabd18b80531de
aryana <aryanorasteh7@gmail.com>
2023-11-16 16:20:17 +0300

    TicketList and db.Json added

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-
+import TicketList from "@/app/tickets/TicketList";
 export default function Tickets(){
     return(
         <main>
-            <h2>Tickets</h2>
+            <nav>
+                <h2>Tickets</h2>
+                <p><small>Currently Open Tickets</small></p>
+            </nav>
+
+            <TicketList/>
         </main>
     )
 }
